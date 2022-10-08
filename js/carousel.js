f96444d1743e177c10e27338ea19b6331cf8d029
Diego Richard Lemos <10diieggos@gmail.com>
2022-10-08 07:06:34 -0300

    Evitando CSS puro
    A funcionalidade de navegação do carousel que estava sendo efetuada por CSS puro foi substituído por JS. Em teoria torna mais Acessível para screen readers

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,28 @@
+var artigo1 = document.querySelector('#new0')
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var artigos = document.querySelectorAll('.listaDeArtigos-item');
+
+
+artigo1.style.display = 'block';
 
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     
+    // Remove a classe 'ativo' do artigo atual
+    artigos.forEach((artigo) => {
+      artigo.style.display = 'none';
+    })
 
-    // Remove classe 'ativo' dos outros botoes
+    // Ativa o artigo que corresponde ao botão clicado
+    let newSelected = this.getAttribute('href')
+    document.querySelector(newSelected).style.display = 'block';
+
+    // Remove classe 'ativo' de todos os botoes
     btns.forEach(function(btnRemoveClass) {
       btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
     })
-
+    // Ativa apenas o botão que foi clicado
     this.classList.add('listaDeArtigos-slider-item--ativo')
   })
 })
